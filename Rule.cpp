8b4d63eb49effaed5c4f0d58ae94450084429207
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-03-04 00:08:06 +0530

    Implement divisibility check for 18 in Rule.cpp
    Logic Used
    Take number input from the user
    Check if number % 2 == 0
    Calculate sum of digits
    Check if sum % 9 == 0
    If both conditions are true → number is divisible by 18
    
    Input: Enter a number: 162
    Output: 162 is divisible by 18

---
+++ Rule.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num, temp, sum = 0;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    bool div2 = num % 2 == 0;
+
+    temp = num;
+
+    while(temp != 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    bool div9 = sum % 9 == 0;
+
+    if(div2 && div9)
+        cout<<num<<" is divisible by 18";
+    else
+        cout<<num<<" is not divisible by 18";
+
+    return 0;
+}
